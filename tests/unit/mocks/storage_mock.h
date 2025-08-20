6f6b264e9723cf03486566910049e92c3e0858bf
Ivan Wagner <ivan.wagner@tecinvent.ch>
2025-08-20 16:14:58 +0200

    mocks: fixed storage mock not compiling

--- tests/unit/mocks/storage_mock.h
+++ tests/unit/mocks/storage_mock.h
@@ -9,7 +9,7 @@
 class MuStorageInterface {
 public:
     virtual ~MuStorageInterface() {}
-    virtual int init();
+    virtual int init() = 0;
     virtual int mount(const mu_storage_config_t *config) = 0;
     virtual int umount(const mu_storage_config_t *config) = 0;
     virtual int file_write(const char *fname, const uint8_t *buf, const size_t size) = 0;
@@ -21,18 +21,19 @@ public:
 
 class MuStorageMock : public MuStorageInterface {
 public:
-
-    MOCK_METHOD(int, init, (), (override));
-    MOCK_METHOD(int, mount, (const mu_storage_config_t *config), (override));
-    MOCK_METHOD(int, umount, (const mu_storage_config_t *config), (override));
-    MOCK_METHOD(int, file_write, (const char *fname, const uint8_t *buf, const size_t size), (override));
-    MOCK_METHOD(int, file_read, (const char *fname, uint8_t *buf, const size_t size), (override));
-    MOCK_METHOD(int, file_remove, (const char *fname), (override));
-    MOCK_METHOD(int, file_exists, (const char *fname), (override));
-
+	virtual ~MuStorageMock() {};
+	MOCK_METHOD(int, init, (), (override));
+	MOCK_METHOD(int, mount, (const mu_storage_config_t *config), (override));
+	MOCK_METHOD(int, umount, (const mu_storage_config_t *config), (override));
+	MOCK_METHOD(int, file_write, (const char *fname, const uint8_t *buf, const size_t size),
+		    (override));
+	MOCK_METHOD(int, file_read, (const char *fname, uint8_t *buf, const size_t size),
+		    (override));
+	MOCK_METHOD(int, file_remove, (const char *fname), (override));
+	MOCK_METHOD(int, file_exists, (const char *fname), (override));
 };
 
-extern const struct mu_storage_if muStorage;
+extern const struct mu_storage_if muStorageMock;
 extern MuStorageMock* MuStorageMockObj;
 
 #endif //MU_STORAGE_MOCK_H_
